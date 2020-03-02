73c4b16ca15601e551a7df91c5000275cc623799
galeblon <s171600@student.pg.edu.pl>
2020-03-02 18:00:48 +0100

    First batch of instructions

--- src/cpu.c
+++ src/cpu.c
@@ -5,6 +5,9 @@
 
 #define INSTRUCTIONS_NUMBER 256
 
+#define _CPU_IS_HALF_CARRY(a, b) ((((a & 0x0F) + (b & 0x0F)) & 0x10) == 0x10)
+#define _CPU_IS_HALF_BORROW(a, b) (((a & 0x0F) - (b & 0x0F)) < 0)
+
 typedef int (*cpu_instruction_t)(void);
 
 static cpu_instruction_t g_instruction_table[INSTRUCTIONS_NUMBER];
@@ -53,11 +56,118 @@ static int _cpu_not_implemented(void)
 	return -1;
 }
 
+// STUB functions, they will be replaced when memory module is done
+static d8 mem_read8(a16 address)
+{
+	return READ_8ROM(address);
+}
+
+static d16 mem_read16(a16 address)
+{
+	return READ_16ROM(address);
+}
+
+static void mem_write8(a16 address, d8 data)
+{
+	data = address + data;
+	return;
+}
+
+static void mem_write16(a16 address, d16 data)
+{
+	data = address + data;
+	return;
+}
+
+// CPU basic instructions
+
 static int _cpu_nop(void)
 {
 	g_registers.PC += 1;
 	return 4;
 }
+
+static int _cpu_ld_bc_d16(void)
+{
+	g_registers.PC += 1;
+	d16 operand = mem_read16(g_registers.PC);
+	g_registers.PC += 2;
+	g_registers.BC = operand;
+	return 12;
+}
+
+static int _cpu_ld_imm_bc_a(void)
+{
+	g_registers.PC += 1;
+	a16 address = g_registers.BC;
+	d8 data = g_registers.A;
+	mem_write8(address, data);
+	return 8;
+}
+
+static int _cpu_inc_bc(void)
+{
+	g_registers.PC += 1;
+	g_registers.BC += 1;
+	return 8;
+}
+
+static int _cpu_inc_b(void)
+{
+	g_registers.PC += 1;
+	d8 left_operand = g_registers.B;
+	d8 right_operand = 0x01;
+	g_registers.B = left_operand + right_operand;
+	g_registers.FLAGS.Z = g_registers.B == 0;
+	g_registers.FLAGS.N = 0;
+	g_registers.FLAGS.H = _CPU_IS_HALF_CARRY(left_operand, right_operand);
+	return 4;
+}
+
+// TODO find reliable source about when half-borrow should be set
+static int _cpu_dec_b(void)
+{
+	g_registers.PC += 1;
+	d8 left_operand = g_registers.B;
+	d8 right_operand = -1;
+	g_registers.B = left_operand - right_operand;
+	g_registers.FLAGS.Z = g_registers.B == 0;
+	g_registers.FLAGS.N = 1;
+	g_registers.FLAGS.H = !_CPU_IS_HALF_BORROW(left_operand, right_operand);
+	return 4;
+}
+
+static int _cpu_ld_b_d8(void)
+{
+	g_registers.PC += 1;
+	d8 operand = mem_read8(g_registers.PC);
+	g_registers.PC += 1;
+	g_registers.B = operand;
+	return 8;
+}
+
+static int _cpu_rlca(void)
+{
+	g_registers.PC += 1;
+	g_registers.FLAGS.Z = 0;
+	g_registers.FLAGS.N = 0;
+	g_registers.FLAGS.H = 0;
+	g_registers.FLAGS.C = (g_registers.A & 0x80) != 0;
+	g_registers.A <<= 1;
+	g_registers.A |= g_registers.FLAGS.C;
+	return 4;
+}
+
+static int _cpu_ld_imm_a16_sp(void)
+{
+	g_registers.PC += 1;
+	a16 address = mem_read16(g_registers.PC);
+	d16 data = g_registers.SP;
+	g_registers.PC += 2;
+	mem_write16(address, data);
+	return 20;
+}
+
 
 static int _cpu_jp_nz_a16(void)
 {
@@ -95,7 +205,16 @@ void cpu_prepare(void)
 	}
 
 	// TODO fill all instructions
-	g_instruction_table[0x0] = _cpu_nop;
+	g_instruction_table[0x00] = _cpu_nop;
+	g_instruction_table[0x01] = _cpu_ld_bc_d16;
+	g_instruction_table[0x02] = _cpu_ld_imm_bc_a;
+	g_instruction_table[0x03] = _cpu_inc_bc;
+	g_instruction_table[0x04] = _cpu_inc_b;
+	g_instruction_table[0x05] = _cpu_dec_b;
+	g_instruction_table[0x06] = _cpu_ld_b_d8;
+	g_instruction_table[0x07] = _cpu_rlca;
+	g_instruction_table[0x08] = _cpu_ld_imm_a16_sp;
+
 	g_instruction_table[0xC2] = _cpu_jp_nz_a16;
 	g_instruction_table[0xC3] = _cpu_jp_a16;
 
