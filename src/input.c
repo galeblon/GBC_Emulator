279166ab95152907404b22859c8847e49a9bafd2
galeblon <32581949+galeblon@users.noreply.github.com>
2020-09-03 18:56:25 +0200

    Update input.c

--- src/input.c
+++ src/input.c
@@ -7,8 +7,6 @@
 static ALLEGRO_EVENT_QUEUE *g_event_queue = NULL;
 static ALLEGRO_EVENT g_event;
 
-void _input_parse_keyboard(struct all_inputs* inputs, bool is_down);
-
 static void _input_error(const char *feature)
 {
 	char *message = logger_get_msg_buffer();
@@ -37,26 +35,6 @@ void input_prepare(void)
 
 	al_register_event_source(g_event_queue, al_get_keyboard_event_source());
 	al_register_event_source(g_event_queue, al_get_joystick_event_source());
-}
-
-void input_check_queue(struct all_inputs* inputs)
-{
-	bool event_exists = al_get_next_event(g_event_queue, &g_event);
-
-	if(event_exists) {
-		switch (g_event.type) {
-		case ALLEGRO_EVENT_KEY_DOWN:
-			_input_parse_keyboard(inputs, true);
-			break;
-		case ALLEGRO_EVENT_KEY_UP:
-			_input_parse_keyboard(inputs, false);
-			break;
-		//TODO parse the joystick
-		//TODO parse emulation specific input, load states etc.
-		default:
-			break;
-		}
-	}
 }
 
 void _input_parse_keyboard(struct all_inputs* inputs, bool is_down)
@@ -88,5 +66,25 @@ void _input_parse_keyboard(struct all_inputs* inputs, bool is_down)
 			break;
 		default:
 			break;
+	}
+}
+
+void input_check_queue(struct all_inputs* inputs)
+{
+	bool event_exists = al_get_next_event(g_event_queue, &g_event);
+
+	if(event_exists) {
+		switch (g_event.type) {
+		case ALLEGRO_EVENT_KEY_DOWN:
+			_input_parse_keyboard(inputs, true);
+			break;
+		case ALLEGRO_EVENT_KEY_UP:
+			_input_parse_keyboard(inputs, false);
+			break;
+		//TODO parse the joystick
+		//TODO parse emulation specific input, load states etc.
+		default:
+			break;
+		}
 	}
 }
