09e93b99425778048435abf61c7edc98c0f12b71
mzal <mzal@ftml.net>
2020-09-15 21:10:58 +0200

    move IO ports and IE regs handling to related modules (closes #69)
    Add a mechanism for registration of memory read/write handlers for particular
    addresses within IO ports range and for IE register.
    
    Refactor joypad, gpu and ints modules to use the new mechanism and use internal
    memory for their own logic instead of calling mem APIs. Dynamically calculate
    joypad register value on read.
    
    Change bgp -> obp in sprite-related gpu functions.

--- src/include/mem_priv.h
+++ src/include/mem_priv.h
@@ -3,10 +3,14 @@
 
 #include"mem.h"
 
+typedef u8 (*mem_read_handler_t)(a16 addr);
+typedef void (*mem_write_handler_t)(a16 addr, u8 data);
+
 u8 mem_vram_read8(int bank, a16 addr);
 void mem_vram_write8(int bank, a16 addr, u8 data);
 
-void mem_set_io_ports(a16 addr, u8 data);
+void mem_register_handlers(a16 addr,
+		mem_read_handler_t r, mem_write_handler_t w);
 
 void mem_h_blank_notify(void);
 
