1e3301142086cb99c0831dac62730ca142d560d6
galeblon <s171600@student.pg.edu.pl>
2020-09-26 17:34:41 +0200

    Up and down bits in wrong order.

--- src/include/joypad.h
+++ src/include/joypad.h
@@ -6,8 +6,8 @@
 struct joypad_register_bits {
 	u8 RIGHT_A : 1;
 	u8 LEFT_B : 1;
-	u8 DOWN_START : 1;
 	u8 UP_SELECT : 1;
+	u8 DOWN_START : 1;
 	u8 SELECT_DIRECTIONS: 1;
 	u8 SELECT_BUTTONS: 1;
 	u8 zero : 2;
