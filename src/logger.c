6cbb35eeccbf984f5457b6d8a0b4c43e01265b95
mzal <mzal@ftml.net>
2020-10-11 23:27:40 +0200

    logger threading fix
    Apparently pthread_cond_signal (and possibly pthread_cond_broadcast) has to be
    send before pthread_mutex_unlock. It makes some sense as it prevents the
    waiting thread from kicking in the very moment the signal is send and let's the
    function finish execution, but I'm not sure of the ordering significance in
    this scenario. Anyway, it appears to be working.

--- src/logger.c
+++ src/logger.c
@@ -16,10 +16,9 @@ typedef struct log_info {
 } log_info;
 
 static log_info g_log_buffer[LOG_BUFFER_SIZE];
-static bool     g_full                        = false,
-		        g_kill                        = false;
-static s8       g_start_index                 = 0,
-		        g_end_index                   = 0;
+static bool g_kill = false;
+static s8 g_start_index = 0,
+	g_end_index = 0;
 
 static pthread_t       g_logger_thread;
 static pthread_cond_t  g_condition_not_empty = PTHREAD_COND_INITIALIZER;
@@ -60,6 +59,16 @@ static inline FILE *_logger_get_output(enum logger_log_type type)
 	return NULL;
 }
 
+static inline bool _logger_is_buffer_full(void)
+{
+	return (g_end_index + 1) % LOG_BUFFER_SIZE == g_start_index;
+}
+
+static inline bool _logger_is_buffer_empty(void)
+{
+	return g_start_index == g_end_index;
+}
+
 void logger_store(
 	enum logger_verbosity verbosity,
 	enum logger_log_type type,
@@ -71,19 +80,15 @@ void logger_store(
 	pthread_mutex_lock(&g_lock);
 
 	//If not full, go
-	if(g_full)
+	if(_logger_is_buffer_full()) {
 		pthread_cond_wait(&g_condition_not_full, &g_lock);
+	}
 
 	g_log_buffer[g_end_index].verbosity = verbosity;
 	g_log_buffer[g_end_index].type = type;
+	g_log_buffer[g_end_index].title[0] = '\0';
 
-	if(title == NULL)
-		strncpy(
-			g_log_buffer[g_end_index].title,
-			"",
-			LOG_TITLE_MAX_SIZE
-		);
-	else
+	if(title)
 		strncpy(
 			g_log_buffer[g_end_index].title,
 			title,
@@ -102,14 +107,12 @@ void logger_store(
 	);
 	va_end(args);
 
-	g_end_index++;
-	g_end_index %= LOG_BUFFER_SIZE;
-	g_full = g_end_index == g_start_index;
-
-	pthread_mutex_unlock(&g_lock);
+	g_end_index = (g_end_index + 1) % LOG_BUFFER_SIZE;
 
 	// We have filled one element of the buffer, so it's not empty
 	pthread_cond_signal(&g_condition_not_empty);
+
+	pthread_mutex_unlock(&g_lock);
 }
 
 void* logger_pop(__attribute__((unused)) void* args)
@@ -118,7 +121,7 @@ void* logger_pop(__attribute__((unused)) void* args)
 		pthread_mutex_lock(&g_lock);
 
 		// If not empty, print
-		if(!g_full && g_start_index == g_end_index)
+		if(_logger_is_buffer_empty())
 			pthread_cond_wait(&g_condition_not_empty, &g_lock);
 
 		FILE *output = _logger_get_output(
@@ -131,11 +134,10 @@ void* logger_pop(__attribute__((unused)) void* args)
 				_logger_log_type_to_text(g_log_buffer[g_start_index].type)
 			);
 
-			if(g_log_buffer[g_start_index].title != NULL)
-				fprintf(output,
-					":\n%s",
-					g_log_buffer[g_start_index].title
-				);
+			fprintf(output,
+				":\n%s",
+				g_log_buffer[g_start_index].title
+			);
 
 			fprintf(output, "\nRegisters:\n");
 			fprintf(
@@ -169,6 +171,7 @@ void* logger_pop(__attribute__((unused)) void* args)
 
 		fprintf(
 			output,
+			"%s",
 			g_log_buffer[g_start_index].message
 		);
 
@@ -176,20 +179,18 @@ void* logger_pop(__attribute__((unused)) void* args)
 			fprintf(output, "\n");
 		}
 
-		g_start_index++;
-		g_start_index %= LOG_BUFFER_SIZE;
-		g_full = false;
+		g_start_index = (g_start_index + 1) % LOG_BUFFER_SIZE;
 
 		// Should we die?
 		if(g_kill) {
-			pthread_mutex_unlock(&g_lock);
 			pthread_cond_broadcast(&g_condition_not_full);
 			pthread_cond_broadcast(&g_condition_not_empty);
+			pthread_mutex_unlock(&g_lock);
 			pthread_exit(NULL);
 		} else {
 			// We have printed one, so it's not full
-			pthread_mutex_unlock(&g_lock);
 			pthread_cond_signal(&g_condition_not_full);
+			pthread_mutex_unlock(&g_lock);
 		}
 
 	}
@@ -199,7 +200,8 @@ void* logger_pop(__attribute__((unused)) void* args)
 
 void logger_prepare(void)
 {
-	pthread_create(&g_logger_thread, NULL, logger_pop, NULL);
+	if (pthread_create(&g_logger_thread, NULL, logger_pop, NULL) != 0)
+		perror("TODO: handle logger thread error");  // TODO
 }
 
 void logger_destroy(void)
